e4fa2b3b28c1de62e4f7f01968c2638335536d7c
naskoap <naskoap@gmail.com>
2016-02-07 23:49:07 -0600

    Added login and logout actions

--- bin/pucture_gallery/views/layout.clj
+++ bin/pucture_gallery/views/layout.clj
@@ -1,7 +1,8 @@
 (ns pucture-gallery.views.layout
   (:require [hiccup.page :refer [html5 include-css]]
             [hiccup.element :refer [link-to]]
-            [noir.session :as session]))
+            [noir.session :as session]
+            [hiccup.form :refer :all]))
 
 (defn base [& content]
   (html5
@@ -13,5 +14,10 @@
 (defn common [& content]
   (base
     (if-let [user (session/get :user)] 
-      [:p user] (link-to "/register" "register"))
+      [:div (link-to "/logout" (str "logout " user))]
+      [:div (link-to "/register" "register")
+       (form-to [:post "/login"]
+                (text-field {:placeholder "username"} "id")
+                (password-field {:placeholder "password"} "pass")
+                (submit-button "login"))])
     content))
